06dcfd26f936efa716d66d6bd29556ce2cef99e7
vagnerhenrik <vagnerhenrique8674@gmail.com>
2016-10-22 15:35:06 -0300

    faz a alternância entre os jogadores

--- versão4.0.py
+++ versão4.0.py
@@ -43,6 +43,7 @@ posições = [
     ]
 
 # Posições que levam ao ganho do jogo
+
 ganho = [
           [ 1, 2, 3], # Linhas
           [ 4, 5, 6],
@@ -64,15 +65,25 @@ jogador = "X" # Começa jogando com X
 jogando = True
 jogadas = 0 # Contador de jogadas - usado para saber se velhou
 while True:
-
+    for t in tabuleiro:  # Imprime o tabuleiro
+        print("".join(t))
+    if not jogando: # Termina após imprimir o último tabuleiro
+        break
+    if jogadas == 9: # Se 9 jogadas foram feitas, todas as posições já foram preenchidas
+        print("Deu velha! Ninguém ganhou.")
+        break
+    print("")
     jogada = int(input("Digite a posição a jogar 1-9 (jogador %s):" % jogador))
     if jogada<1 or jogada>9:
         print("Posição inválida")
         continue
     # Verifica se a posição está livre
     if tabuleiro[posições[jogada][0]][posições[jogada][1]] != " ":
-        print("Posição ocupada.")
+        print("Posição ocupada.");
         continue
+
+    jogador = "X" if jogador == "O" else "O" # Alterna jogador
+    jogadas +=1 # Contador de jogadas
 
 
 
